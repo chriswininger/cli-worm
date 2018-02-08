e4d97bcd24c8cb4dd0fb334fc27b3b3fa51756f3
chriswininger <chris.wininger@gmail.com>
2018-02-08 16:19:47 -0500

    corrected chapter read function
    it now handles chapter lists with nesting such as
     books which have parts and chapters

--- utils/utils.js
+++ utils/utils.js
@@ -21,16 +21,31 @@ module.exports = {
                 if (err)
                     return reject(err)
 
-                const obj = parse(xml);
-                const list = obj.root.children.find(node => node.name === 'navMap')
-                    .children.filter(node => node.name === 'navPoint')
-                    .map(navPoint => {
-                        return {
-                            text: navPoint.children.find(p => p.name === 'navLabel').children.find(p => p.name === 'text').content,
-                            link: navPoint.children.find(p => p.name === 'content').attributes.src
-                        }
-                    });
+                const paddingForParts = '  '
+                const extractChapters = (root, padding) => {
+                    if (typeof padding === 'undefined')
+                        padding = ''
 
+                    let list = []
+
+                    root.children.filter(node => node.name === 'navPoint')
+                        .forEach(navPoint => {
+                            list.push({
+                                text: padding + navPoint.children.find(p => p.name === 'navLabel').children.find(p => p.name === 'text').content,
+                                link: navPoint.children.find(p => p.name === 'content').attributes.src
+                            })
+
+                            //console.log(navPoint)
+
+                            // recur back for any nested chapters
+                            list = list.concat(extractChapters(navPoint, padding + paddingForParts))
+                        })
+
+                    return list
+                }
+
+                const obj = parse(xml);
+                const list = extractChapters(obj.root.children.find(node => node.name === 'navMap'))
                 resolve(list)
             })
         })
