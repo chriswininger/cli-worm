163d04841ca5ce605fb1417b54612615a9c9db72
chriswininger <chris.wininger@gmail.com>
2018-02-17 18:20:08 -0500

    fixed logger to only output when Leve=debug

---
+++ utils/utils.logger.js
@@ -0,0 +1,17 @@
+const { configure, getLogger } = require('log4js');
+require('dotenv').config()
+
+module.exports = {
+    getLogger: (level) => {
+        level = level || 'debug'
+        configure({
+            appenders: { debug: { type: 'file', filename: 'debug.log' } },
+            categories: { default: { appenders: ['debug'], level: 'debug' } }
+        });
+        const logger = getLogger(level);
+        logger.level = process.env.Level || 'OFF';
+
+        logger.debug('!!! grr: ' + process.env.Level)
+        return logger
+    }
+}
